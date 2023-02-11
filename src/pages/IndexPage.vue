3cfad245942d706ed4b3b484b96cf6ae14130e70
Darya <daryabsb@gmail.com>
2023-02-11 16:32:04 +0300

    First commit

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,18 @@
+<template>
+  <q-page class="flex flex-center">
+    <q-btn-group push>
+      <q-btn color="red" glossy text-color="white" label="First" icon="verified_user" />
+      <q-btn color="amber" glossy text-color="black" push label="Second" />
+      <q-btn color="orange" glossy text-color="black" push label="Third" />
+    </q-btn-group>
+
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'IndexPage'
+})
+</script>
