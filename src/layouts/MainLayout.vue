85174ee8c6f3de63ff523dca9c34e6cb9f5cbcaa
Darya <daryabsb@gmail.com>
2023-02-12 13:58:44 +0300

    Adding tailwind

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,53 +1,90 @@
 <template>
-  <q-layout view="lHh Lpr lFf">
-    <q-header elevated>
+  <q-layout view="hHh lpR fFf">
+    <q-header elevated class="q-py-md">
       <q-toolbar>
-        <q-btn
-          flat
-          dense
-          round
-          icon="menu"
-          aria-label="Menu"
-          @click="toggleLeftDrawer"
-        />
+        <q-btn flat dense round icon="menu" aria-label="Menu" @click="toggleLeftDrawer" />
 
         <q-toolbar-title>
-          Quasar App
+          Quasar test App
         </q-toolbar-title>
-
+        <q-space></q-space>
+        <q-btn-toggle v-model="model" flat stretch toggle-color="yellow" :options="options" size="lg"></q-btn-toggle>
+        <q-space></q-space>
         <div>Quasar v{{ $q.version }}</div>
       </q-toolbar>
     </q-header>
 
-    <q-drawer
-      v-model="leftDrawerOpen"
-      show-if-above
-      bordered
-    >
-      <q-list>
-        <q-item-label
-          header
-        >
-          Essential Links
-        </q-item-label>
+    <q-drawer v-model="leftDrawerOpen" show-if-above :mini="!leftDrawerOpen || miniState" @click.capture="drawerClick"
+      :breakpoint="500" bordered class="bg-grey-3">
+      <template v-slot:mini>
+        <q-scroll-area class="fit mini-slot cursor-pointer">
+          <div class="q-py-lg">
+            <div class="column items-start">
+              <q-icon name="inbox" color="blue" class="mini-icon" />
+              <q-icon name="star" color="orange" class="mini-icon" />
+              <q-icon name="send" color="purple" class="mini-icon" />
+              <q-icon name="drafts" color="teal" class="mini-icon" />
+            </div>
+          </div>
+        </q-scroll-area>
+      </template>
 
-        <EssentialLink
-          v-for="link in essentialLinks"
-          :key="link.title"
-          v-bind="link"
-        />
-      </q-list>
+      <q-scroll-area class="fit">
+        <q-list padding>
+          <q-item clickable v-ripple>
+            <q-item-section>
+              Inbox
+            </q-item-section>
+          </q-item>
+
+          <q-item active clickable v-ripple>
+            <q-item-section>
+              Star
+            </q-item-section>
+          </q-item>
+
+          <q-item clickable v-ripple>
+            <q-item-section>
+              Send
+            </q-item-section>
+          </q-item>
+
+          <q-item clickable v-ripple>
+            <q-item-section>
+              Drafts
+            </q-item-section>
+          </q-item>
+        </q-list>
+      </q-scroll-area>
+
+      <!--
+          in this case, we use a button (can be anything)
+          so that user can switch back
+          to mini-mode
+        -->
+      <div class="q-mini-drawer-hide absolute" style="top: 15px; right: -17px">
+        <q-btn dense round unelevated color="accent" icon="chevron_left" @click="miniState = true" />
+      </div>
     </q-drawer>
-
     <q-page-container>
       <router-view />
     </q-page-container>
+    <q-footer bordered class="bg-grey-8 text-white q-py-md q-px-lg">
+      <q-toolbar>
+        <q-toolbar-title>
+          <q-avatar>
+            <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg">
+          </q-avatar>
+          <div>Title</div>
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-footer>
   </q-layout>
 </template>
 
 <script>
 import { defineComponent, ref } from 'vue'
-import EssentialLink from 'components/EssentialLink.vue'
+// import EssentialLink from 'components/EssentialLink.vue'
 
 const linksList = [
   {
@@ -98,19 +135,54 @@ export default defineComponent({
   name: 'MainLayout',
 
   components: {
-    EssentialLink
+    // EssentialLink
   },
 
-  setup () {
+  setup() {
     const leftDrawerOpen = ref(false)
+    const miniState = ref(false)
 
     return {
       essentialLinks: linksList,
       leftDrawerOpen,
-      toggleLeftDrawer () {
+      drawer: ref(false),
+      miniState,
+      toggleLeftDrawer() {
         leftDrawerOpen.value = !leftDrawerOpen.value
-      }
+      },
+      drawerClick(e) {
+        // if in "mini" state and user
+        // click on drawer, we switch it to "normal" mode
+        if (miniState.value) {
+          miniState.value = false
+
+          // notice we have registered an event with capture flag;
+          // we need to stop further propagation as this click is
+          // intended for switching drawer to "normal" mode only
+          e.stopPropagation()
+        }
+      },
+      model: ref('one'),
+
+      options: [
+        { label: 'One', value: 'one' },
+        { label: 'Two', value: 'two' },
+        { label: 'Three', value: 'three' }
+      ]
     }
   }
 })
 </script>
+<style lang="sass" scoped>
+.mini-slot
+  transition: background-color .28s
+  &:hover
+    background-color: rgba(0, 0, 0, .04)
+
+.mini-icon
+  font-size: 1.718em
+  padding: 2px 16px
+
+  & + &
+    margin-top: 18px
+</style>
